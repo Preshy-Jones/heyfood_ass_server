0be183b8392713ade0495e3b5b8ef5398aab3cff
Precious Adedibu <adedibuprecious@gmail.com>
2023-10-24 01:38:17 +0100

    set up express app

---
+++ src/routes/testroute.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+import validateResource from "../middlewares/validateResource";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello Test route!");
+});
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import express from "express";
+// import compression from "compression";
+// import cookieparser from "cookie-parser";
+import { errorHandler, notFoundHandler } from "./middlewares/errors";
+import validateResource from "./middlewares/validateResource";
+import testRoute from "./routes/testroute";
+import connectDB from "./config/db";
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// app.use(compression());
+// app.use(cookieparser());
+
+app.get("/", (req: Request, res: any) => {
+  res.json({
+    msg: "Test Delivery Service...",
+  });
+});
+
+app.use("/api/testroute", testRoute);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+connectDB();
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`);
+});
